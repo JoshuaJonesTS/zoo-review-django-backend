f6d32d69316dd9b0d28a4f67fcd2d34107a9252c
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-02 17:02:20 -0700

    Delete secret key
    If this were a "real" project, a new key would need to be created and put in the .env file.

--- zooreviewbackend/zooreviewbackend/settings.py
+++ zooreviewbackend/zooreviewbackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9x@1!oz_(499k&8*)jlmma=y3aotqhiwystqn^p3@+=3vv9e4t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
